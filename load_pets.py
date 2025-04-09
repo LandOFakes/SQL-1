cef34bac3c795913ab81a7b6323b09d2eff03370
LandOFakes <P.jbrown198@gmail.com>
2025-04-08 21:14:47 -0400

    Update load_pets.py

--- load_pets.py
+++ load_pets.py
@@ -1,4 +1,29 @@
 import sqlite3
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS person (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    age INTEGER
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS pet (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT,
+    age INTEGER,
+    dead INTEGER
+);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS person_pet (
+    person_id INTEGER,
+    pet_id INTEGER
+);
+""")
 
 # Connect to pets.db
 conn = sqlite3.connect('pets.db')
